1f5fe8ed511653ac24155a95f8f7fe39add6ce8f
anayeremeiko <nastyaeremeyko@gmail.com>
2018-10-11 01:14:06 +0300

    multyfileassembly

---
+++ NET1.A.2018.Yeremeiko.01/Products/Cheese.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Products
+{
+    /// <summary>
+    /// This is Cheese class
+    /// </summary>
+    public class Cheese
+    {
+        /// <summary>
+        /// cheese name
+        /// </summary>
+        private string name;
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="name">cheese name</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when the cheese name is null 
+        /// </exception>
+        public Cheese(string name)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            this.name = name;
+        }
+
+        /// <summary>
+        /// The info method.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        public string Info()
+        {
+            return $"Cheese {name}";
+        }
+    }
+}
---
+++ NET1.A.2018.Yeremeiko.01/Products/Vegetable.cs
@@ -0,0 +1,40 @@
+﻿namespace Products
+{
+    /// <summary>
+    /// Class describing a vegetable
+    /// </summary>
+    public class Vegetable
+    {
+        /// <summary>
+        /// vegetable color
+        /// </summary>
+        private string color;
+
+        /// <summary>
+        /// weight of vegetable
+        /// </summary>
+        private double weight;
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="color">vegetable color</param>
+        /// <param name="weight">weight of vegetable</param>
+        public Vegetable(string color, double weight)
+        {
+            this.color = color;
+            this.weight = weight;
+        }
+
+        /// <summary>
+        /// The info method.
+        /// </summary>
+        /// <returns>
+        /// Color and weight of vegetable.
+        /// </returns>
+        public string Info()
+        {
+            return $"This vegetable is {color}, weight = {weight}";
+        }
+    }
+}
